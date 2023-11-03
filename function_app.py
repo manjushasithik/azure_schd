40aa736b167a1a7a3ae269e99eba9d50fa82e29c
manjushasithik <61084805+manjushasithik@users.noreply.github.com>
2023-11-03 18:33:51 +0530

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,32 +1,32 @@
 
-# import datetime
-# import logging
-# import azure.functions as func
-# import requests
-# app = func.FunctionApp()
-
-# @app.function_name(name="mytimer")
-# @app.schedule(schedule="0 */1 * * * *", 
-#               arg_name="mytimer",
-#               run_on_startup=True) 
-# def test_function(mytimer: func.TimerRequest) -> None:
-#     utc_timestamp = datetime.datetime.utcnow().replace(
-#         tzinfo=datetime.timezone.utc).isoformat()
-#     if mytimer.past_due:
-#         logging.info('The timer is past due!')
-#     response = requests.get("https://detention-api.azurewebsites.net/test")
-    
-    
-    
+import datetime
 import logging
 import azure.functions as func
 import requests
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 */1 * * * *", arg_name="myTimer", run_on_startup=True,
-              use_monitor=False) 
-def bbb(myTimer: func.TimerRequest) -> None:
-    if myTimer.past_due:
+@app.function_name(name="mytimer")
+@app.schedule(schedule="0 */1 * * * *", 
+              arg_name="mytimer",
+              run_on_startup=True) 
+def test_function(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc).isoformat()
+    if mytimer.past_due:
         logging.info('The timer is past due!')
     response = requests.get("https://detention-api.azurewebsites.net/test")
-    logging.info('Python timer trigger fwunction executed.')
+    
+    
+    
+# import logging
+# import azure.functions as func
+# import requests
+# app = func.FunctionApp()
+
+# @app.schedule(schedule="0 */1 * * * *", arg_name="myTimer", run_on_startup=True,
+#               use_monitor=False) 
+# def bbb(myTimer: func.TimerRequest) -> None:
+#     if myTimer.past_due:
+#         logging.info('The timer is past due!')
+#     response = requests.get("https://detention-api.azurewebsites.net/test")
+#     logging.info('Python timer trigger fwunction executed.')
