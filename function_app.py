fc2eb6d68bb89a7aa47cffda030265faa212dcc1
manjushasithik <61084805+manjushasithik@users.noreply.github.com>
2023-12-07 18:45:56 +0530

    13:20

--- function_app.py
+++ function_app.py
@@ -6,7 +6,7 @@ import requests
 app = func.FunctionApp()
 
 @app.function_name(name="mytimer")
-@app.schedule(schedule="0 35 17 * * *", 
+@app.schedule(schedule="0 20 13 * * *", 
               arg_name="mytimer",
               run_on_startup=True) 
 def test_function(mytimer: func.TimerRequest) -> None:
@@ -14,8 +14,26 @@ def test_function(mytimer: func.TimerRequest) -> None:
         tzinfo=datetime.timezone.utc).isoformat()
     if mytimer.past_due:
         logging.info('The timer is past due!')
-    response = requests.get("https://detention-api.azurewebsites.net/test")
-    
+        
+    username = "cbm"
+    password = "cbm"
+
+    token_url = "https://smartmaintenance.azurewebsites.net/token"
+    token_response = requests.post(
+        token_url,
+        data={"username": username, "password": password},
+    )
+    token_data = token_response.json()
+    access_token = token_data["access_token"]
+
+    predict_url = "https://smartmaintenance.azurewebsites.net/forecast-14days"
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+  
+
+    prediction_response = requests.post(predict_url, headers=headers) #, json=input_data
+      
+        
     
     
 # import logging
